67b02b363d4311528d6e5b2c292dcf7576c5ba75
Marcin Lech <arcinech@gmail.com>
2022-03-28 09:18:43 +0200

    Add: initPage change and initPlugin

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
-import { settings } from './settings';
+import { settings, select, classNames } from './settings.js';
+import Home from './components/home.js';
+import Songs from './components/Songs.js';
 
 const app = {
   initData: function(){
-    this.data = {};
+    const thisApp = this;
+    thisApp.data = {};
 
     const url = settings.db.url + '/' + settings.db.songs;
 
@@ -11,13 +14,86 @@ const app = {
         return rawResponse.json();
       })
       .then(function(songs){
-        this.data.songs = songs;
+        console.log(thisApp);
+        thisApp.data.songs = songs;
+
+        thisApp.initHome(thisApp.data.songs);
       });
+    return thisApp.data;
+  },
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    console.log(idFromHash);
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(const page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+
+        window.location.hash = '#/' + id;
+      });
+    }
+
+  },
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    for(let page of thisApp.pages){
+      page.classList.toggle(
+        classNames.pages.active,
+        page.id == pageId
+      );
+    }
+
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.pages.active,
+        link.getAttribute('href')  == '#' + pageId
+      );
+    }
+  },
+
+  initHome: function(){
+    const thisApp = this;
+
+    for(let songData in thisApp.data.songs){
+      new Songs(thisApp.data.songs[songData].id, thisApp.data.songs[songData]);
+    }
+  },
+
+  initPlugin: function(){
+    // eslint-disable-next-line no-undef
+    GreenAudioPlayer.init({
+      selector: '.player', // inits Green Audio Player on each audio container that has class "player"
+      stopOthersOnPlay: true
+    });
   },
 
   init: function(){
+    const thisApp = this;
 
-    this.initData();
+    thisApp.initData();
+    thisApp.initPages();
+    thisApp.initPlugin();
   }
 };
 
