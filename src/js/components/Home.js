c8491e9a75788bb6e552022645fbcd5a085077fc
Marcin Lech <arcinech@gmail.com>
2022-03-29 20:19:33 +0200

    Add: Home page functionality

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,22 +1,43 @@
-import { select } from '../settings.js';
+import { select, templates } from '../settings.js';
 import AudioPlugin from './AudioPlugin.js';
+import Songs from './Songs.js';
+import { utils } from '../utils.js';
 
 class Home{
   constructor(songs, authors) {
-    this.data = {};
-    console.log(this.data[0]);
+    this.songs = songs;
+    this.authors = authors;
+    console.log(authors);
+    this.renderHome();
+    this.getElements();
+    this.renderSongs();
+
+    new AudioPlugin(select.home.initPlugin);
   }
 
-  // render(data){
-  // random song? maximum songs on home page 4?
-  //   for(song of data){
-  //     if(data[song] < )
-  //   }
-  // }
+  renderHome(){
+    this.dom = {};
+    this.dom.wrapper = document.querySelector(select.containerOf.homePage);
+    console.log(this.dom.wrapper);
+    const generetedHTML = templates.homePage();
+    console.log(generetedHTML);
+    this.dom.wrapper.innerHTML = generetedHTML;
+    console.log(this.dom.wrapper.innerHTML);
+  }
 
   getElements(){
-    this.dom = {};
-    this.dom.wrapper = document.querySelector(select.containerOf.home);
+    this.dom.homeSongs = this.dom.wrapper.querySelector(select.home.song);
+    this.dom.songWrapper = this.dom.wrapper.querySelector(select.all.songsWrapper);
+  }
+
+  renderSongs(){
+    for(let song of this.songs){
+
+      this.data = utils.songData(song, this.authors);
+
+      new Songs(this.data, this.dom.songWrapper);
+    }
+
   }
 
 }
