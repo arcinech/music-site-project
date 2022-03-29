eacdfd567b7c80c4c3b14fcc925706ee097a3d51
Marcin Lech <arcinech@gmail.com>
2022-03-29 20:19:19 +0200

    Add: new utils

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,1 +1,20 @@
-const utils = {};
+export const utils = {};
+
+
+utils.findAuthor = function(song, authors){
+  //return author name based on author id in songs database
+  for(let name of authors){
+    if(song.author == name.id) return name.author;
+  }
+};
+
+utils.songData = function(song, authors){
+  const data = {};
+  data.title = song.title;
+  data.author = utils.findAuthor(song, authors);
+  data.filename = song.filename;
+  data.categories = song.categories;
+  data.ranking = song.ranking;
+
+  return data;
+};
