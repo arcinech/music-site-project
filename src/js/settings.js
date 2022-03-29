bd124eab3ffef290fee64127112d33eaf4399ddd
Marcin Lech <arcinech@gmail.com>
2022-03-29 20:20:16 +0200

    Extend settings

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,6 +2,7 @@ export const settings = {
   db: {
     url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     songs: 'songs',
+    authors: 'authors',
   },
 };
 
@@ -14,15 +15,23 @@ export const select = {
   },
   containerOf: {
     pages: '#pages',
-    home: '.home',
-    search: '.search',
-    discovery: '.search'
+    homePage: '.home-page',
+    search: '.search-page',
+    discovery: '.discover'
+  },
+  all: {
+    songsWrapper: '.songs',
   },
   nav: {
     links: '.main-nav a',
   },
   home: {
+    songs: '#home-song',
     button: '.btn-home',
+    initPlugin: '.home-page .player'
+  },
+  discovery: {
+    initPlugin: '.discover .player'
   },
   splash: {
     title: '.splash-title',
@@ -41,10 +50,10 @@ export const classNames = {
     active: 'active',
   },
 };
-
+console.log(document.querySelector(select.templateOf.homePage).innerHTML);
 export const templates = {
-  homePage: Handlebars.compile(document.querySelector(select.templatesOf.homePage)),
-  searchPage: Handlebars.compile(document.querySelector(select.templateOf.searchPage)),
-  discoveryPage: Handlebars.compile(document.querySelector(select.templateOf.discoverPage)),
-  musicPlayer: Handlebars.compile(document.querySelector(select.templateOf.musicPlayer)),
+  homePage: Handlebars.compile(document.querySelector(select.templateOf.homePage).innerHTML),
+  searchPage: Handlebars.compile(document.querySelector(select.templateOf.searchPage).innerHTML),
+  discoveryPage: Handlebars.compile(document.querySelector(select.templateOf.discoverPage).innerHTML),
+  musicPlayer: Handlebars.compile(document.querySelector(select.templateOf.musicPlayer).innerHTML),
 };
